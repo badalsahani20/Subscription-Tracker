79c11f59670f4ff7a02fe18a7691ef6cb85a2cf8
badalsahani20 <badalrani20@gmail.com>
2025-10-28 23:52:27 +0530

    🚀 Final version: Subscription Tracker completed with email reminders

--- config/env.js
+++ config/env.js
@@ -11,5 +11,7 @@ export const {
   ARCJET_KEY,
   ARCJET_ENV,
   QSTASH_URL,
-  QSTASH_TOKEN
+  QSTASH_TOKEN,
+  SERVER_URL,
+  EMAIL_PASSWORD
 } = process.env;
--- controller/subscription.controller.js
+++ controller/subscription.controller.js
@@ -1,26 +1,26 @@
 import Subscription from "../models/subscription.model.js";
+import { workflowClient } from '../config/upstash.js'
+import { SERVER_URL } from '../config/env.js'
 export const createSubscription = async (req, res, next) => {
   try {
-    const {name} = req.body;
-    const existingSubscription = await Subscription.findOne({
-      user: req.user._id,
-      name,
-      status: "ACTIVE",
-    });
-
-    if(existingSubscription) {
-      return res.status(400).json({
-        success: false,
-        message: `You already have an active ${name} plan.`
-      });
-    }
-
     const subscription = await Subscription.create({
       ...req.body,
       user: req.user._id,
     });
 
-    res.status(201).json({ success: true, data: subscription });
+    const { workflowRunId} = await workflowClient.trigger({
+      url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
+      body: {
+        subscriptionId: subscription.id,
+      },
+      headers: {
+        'content-type': 'application/json',
+      },
+      retries: 0,
+    })
+    // console.log("Workflow Trigger Response:", response);
+
+    res.status(201).json({ success: true, data: subscription, workflowRunId });
   } catch (error) {
     next(error);
   }
